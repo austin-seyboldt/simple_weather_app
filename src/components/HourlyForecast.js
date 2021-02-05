f8dee80d09505174a8ac5d42e1d427a5a7f72ecb
Austin Seyboldt <seyboldt.austin@gmail.com>
2021-02-04 18:48:54 -0800

    possibly fixed apple nan bug

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -1,11 +1,12 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "../css/HourlyForecast.css";
 import { GlobalContext } from "./GlobalContext";
 import { DaysofTheWeek } from "../DaysofTheWeek";
 
-const ForecastHour = ({ forecast }) => {
+const ForecastHour = ({ forecast, isApple, convertDateForApple }) => {
     const { isCelsius } = useContext(GlobalContext);
-    const hour = new Date(forecast.time);
+    const inTime = isApple ? convertDateForApple(forecast.time) : forecast.time;
+    const hour = new Date(inTime);
     const time = hour.getHours();
     let temp = isCelsius ? forecast.temp_c : forecast.temp_f;
     temp = parseInt(temp);
@@ -24,10 +25,16 @@ const ForecastHour = ({ forecast }) => {
 const HourlyForecast = () => {
     const { forecast, localTime, isDarkMode } = useContext(GlobalContext);
     const [hourCount, setHourCount] = useState(10);
+    const [isApple, setIsApple] = useState(false);
+
+    const convertDateForApple = (date) => {
+        return date.replace(/-/g, "/");
+    };
 
     const getForecastHours = () => {
         let forecastHours = forecast[0].hour.filter((hour) => {
-            let newHour = new Date(hour.time).getHours();
+            const time = isApple ? convertDateForApple(hour.time) : hour.time;
+            let newHour = new Date(time).getHours();
             return newHour >= localTime && newHour < localTime + 10;
         });
         if (hourCount - forecastHours.length > 0) {
@@ -39,12 +46,25 @@ const HourlyForecast = () => {
         return forecastHours;
     };
 
+    useEffect(() => {
+        if (navigator.userAgent.match(/(iPhone | iPod | iPad)/) != null) {
+            setIsApple(true);
+        }
+    }, []);
+
     return (
         <div className={`hourly__forecast ${isDarkMode ? "dark__mode" : ""}`}>
             <h1 className="hourly__forecast--header">10 Hour Forecast</h1>
             <ul className="hourly__forecast__list">
                 {getForecastHours().map((hour) => {
-                    return <ForecastHour forecast={hour} key={hour.time} />;
+                    return (
+                        <ForecastHour
+                            isApple={isApple}
+                            convertDateForApple={convertDateForApple}
+                            forecast={hour}
+                            key={hour.time}
+                        />
+                    );
                 })}
             </ul>
         </div>
